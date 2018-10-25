aa7cd0efa25eb843ab2a912761fd4a082195b106
iben12 <iben@iben.hu>
2018-10-25 17:39:32 +0200

    ops(extension): rename module, update compose file SUITEDEV-14879

--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -1,6 +1,13 @@
 <?php
 class Emartech_Emarsys_IndexController extends Mage_Core_Controller_Front_Action
 {
+    /**
+     * Index action
+     *
+     * @access public
+
+     * @return void
+     */
     public function indexAction()
     {
         echo "Hello Emarsys";
