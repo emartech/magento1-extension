f4eb418ad8f0d813b8a4ffe872b974640441cc27
iben12 <iben@iben.hu>
2018-11-27 13:16:01 +0100

    test(websites-endpoint): add test for websites endpoint SUITEDEV-15354

---
+++ dev/test/websites/get.spec.js
@@ -0,0 +1,14 @@
+'use strict';
+
+describe('Websites endpoint', function() {
+  it('should return websites information', async function() {
+    const expectedWebsites = [
+      { id: '0', code: 'admin', name: 'Admin', default_group_id: '0' },
+      { id: '1', code: 'base', name: 'Main Website', default_group_id: '1' }
+    ];
+
+    const websites = await this.magentoApi.execute('websites', 'get');
+
+    expect(websites).to.eql(expectedWebsites);
+  });
+});
