da8891f5ec65968283023071fb15daa5e2ef4d09
Norbert Pospischek <npospischek@emarsys.com>
2019-01-17 15:11:00 +0100

    feat(sales-marketing-event): add guest order tests
    Co-Authored-By: gaborbencsik <gaborbencsik@users.noreply.github.com>

--- dev/test/cypress/integration/marketing-events/sales.spec.js
+++ dev/test/cypress/integration/marketing-events/sales.spec.js
@@ -3,27 +3,53 @@
 const getUid = () => Math.round(Math.random() * 100000);
 
 describe('Marketing Events - Sales', function() {
-  const createTestOrder = ({ email, password }) => {
+  const checkoutWithProduct = () => {
     cy.visit('/accessories/jewelry/swing-time-earrings.html');
     cy.get('.add-to-cart-wrapper .button.btn-cart').click();
     cy.get('.page-title .button.btn-proceed-checkout.btn-checkout').click();
-    cy.get('#login-email').type(email);
-    cy.get('#login-password').type(password);
-    cy.get('.col-2 > .buttons-set > .button').click();
+  };
 
+  const fillBillingData = () => {
     cy.get('select[id="billing:country_id"]').select('HU');
     cy.get('input[id="billing:street1"]').type('Adress street 42');
     cy.get('input[id="billing:city"]').type('Fancycity');
     cy.get('input[id="billing:postcode"]').type('1234');
     cy.get('input[id="billing:telephone"]').type('705551234');
     cy.get('#checkout-step-billing .buttons-set .button').click();
+  };
 
+  const finishOrder = () => {
     cy.get('#s_method_flatrate_flatrate').check();
     cy.get('#checkout-step-shipping_method .buttons-set .button').click();
 
     cy.get('#checkout-step-payment .buttons-set .button').click();
 
     cy.get('#checkout-review-submit .buttons-set .button').click();
+  };
+
+  const createOrder = ({ email, password }) => {
+    checkoutWithProduct();
+
+    cy.get('#login-email').type(email);
+    cy.get('#login-password').type(password);
+    cy.get('.col-2 > .buttons-set > .button').click();
+
+    fillBillingData();
+    finishOrder();
+    cy.wait(1000);
+  };
+
+  const createGuestOrder = ({ email, firstName, lastName }) => {
+    checkoutWithProduct();
+
+    cy.get('#onepage-guest-register-button').click();
+
+    cy.get('input[id="billing:firstname"]').type(firstName);
+    cy.get('input[id="billing:lastname"]').type(lastName);
+    cy.get('input[id="billing:email"]').type(email);
+
+    fillBillingData();
+    finishOrder();
     cy.wait(1000);
   };
 
@@ -47,6 +73,13 @@ describe('Marketing Events - Sales', function() {
     cy.get('.scalable.save.submit-button[title="Refund Offline"]').click();
   };
 
+  const shouldCreateAllSalesEvents = eventData => {
+    cy.shouldCreateEvent('sales_email_order_template', eventData);
+    cy.shouldCreateEvent('sales_email_invoice_template', eventData);
+    cy.shouldCreateEvent('sales_email_shipment_template', eventData);
+    cy.shouldCreateEvent('sales_email_creditmemo_template', eventData);
+  };
+
   context('with collectMarketingEvents disabled', function() {
     before(() => {
       cy.task('setConfig', { websiteId: 1, config: { collectMarketingEvents: 'disabled' } });
@@ -57,7 +90,18 @@ describe('Marketing Events - Sales', function() {
       const password = 'SalesEvents123';
 
       cy.registerCustomer({ email, password });
-      createTestOrder({ email, password });
+      createOrder({ email, password });
+      fulfillLatestOrder();
+
+      cy.shouldNotExistsEvents();
+    });
+
+    it('should create guest sales events', function() {
+      const email = `cypress-marketing-off-sales${getUid()}@guest-events.com`;
+      const firstName = 'User';
+      const lastName = 'Guest';
+
+      createGuestOrder({ email, firstName, lastName });
       fulfillLatestOrder();
 
       cy.shouldNotExistsEvents();
@@ -106,7 +150,7 @@ describe('Marketing Events - Sales', function() {
         const password = 'SalesEvents123';
 
         cy.registerCustomer({ email, password });
-        createTestOrder({ email, password });
+        createOrder({ email, password });
         fulfillLatestOrder();
 
         cy.shouldNotExistsEvents(['customer_new_account_registered']);
@@ -118,13 +162,27 @@ describe('Marketing Events - Sales', function() {
       const password = 'SalesEvents123';
 
       cy.registerCustomer({ email, password });
-      createTestOrder({ email, password });
+      createOrder({ email, password });
       fulfillLatestOrder();
 
-      cy.shouldCreateEvent('sales_email_order_template', { customer: { email } });
-      cy.shouldCreateEvent('sales_email_invoice_template', { customer: { email } });
-      cy.shouldCreateEvent('sales_email_shipment_template', { customer: { email } });
-      cy.shouldCreateEvent('sales_email_creditmemo_template', { customer: { email } });
+      shouldCreateAllSalesEvents({ customer: { email } });
+    });
+
+    it('should create guest sales events', function() {
+      const email = `cypress-marketing-on-sales${getUid()}@guest-events.com`;
+      const firstName = 'User';
+      const lastName = 'Guest';
+
+      createGuestOrder({ email, firstName, lastName });
+      fulfillLatestOrder();
+
+      const guestEventData = {
+        customerEmail: email,
+        customerName: `${firstName} ${lastName}`,
+        is_guest: '1'
+      };
+
+      shouldCreateAllSalesEvents(guestEventData);
     });
   });
 });
