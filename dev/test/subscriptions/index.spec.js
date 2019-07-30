61a5d07e621ba46b05f1760fe2c23d0437d3fcc2
Norbert Pospischek <npospischek@emarsys.com>
2019-07-30 12:42:34 +0200

    fix(subscriptions): add/fix tests CDPID-1737
    Co-Authored-By: erko256 <erko256@users.noreply.github.com>

--- dev/test/subscriptions/index.spec.js
+++ dev/test/subscriptions/index.spec.js
@@ -15,7 +15,8 @@ const createSubscriptionSetter = db => {
     return await db('newsletter_subscriber').insert({
       subscriber_email: email,
       subscriber_status: status,
-      customer_id: customerId
+      customer_id: customerId,
+      store_id: storeId
     });
   };
 };
@@ -24,20 +25,42 @@ const isSubscribed = subscription => {
   return subscription !== undefined && subscription.subscriber_status === 1;
 };
 
+let customerEmail;
+let customerEmail2;
+let customerId;
+let customerId2;
 const noCustomerEmail = 'no-customer@a.com';
 const noCustomerEmail2 = 'still-no-customer@a.com';
-const customerEmail = 'roni_cost@example.com';
-const customerId = 1;
 const websiteId = 1;
 const storeId = 1;
+
+const GUEST_CUSTOMER_ID = 0;
+const MAGENTO_SUBSCRIBE_STATUS = 1;
+const MAGENTO_UNSUBSCRIBE_STATUS = 3;
 
 describe('Subscriptions api', function() {
   let subscriptionFor;
   let createSubscription;
 
-  before(function() {
+  before(async function() {
     subscriptionFor = createSubscriptionGetter(this.db);
     createSubscription = createSubscriptionSetter(this.db);
+
+    const customer2 = await this.createCustomer({
+      group_id: 0,
+      dob: '1977-11-12',
+      email: 'subscription-tester@yolo.net',
+      firstname: 'Tester',
+      lastname: 'Subscription',
+      store_id: storeId,
+      website_id: websiteId,
+      password: 'Password1234'
+    });
+
+    customerEmail = this.customer.email;
+    customerId = this.customer.entityId;
+    customerEmail2 = customer2.email;
+    customerId2 = customer2.entityId;
   });
 
   describe('update', function() {
@@ -52,17 +75,60 @@ describe('Subscriptions api', function() {
         expect(await subscriptionFor(noCustomerEmail)).to.be.undefined;
 
         await this.magentoApi.execute('subscriptions', 'update', {
-          subscriptions: [{ subscriber_email: noCustomerEmail, subscriber_status: true }]
+          subscriptions: [
+            {
+              subscriber_email: noCustomerEmail,
+              subscriber_status: true,
+              website_id: websiteId,
+              customer_id: GUEST_CUSTOMER_ID
+            }
+          ]
         });
+
         expect(await subscriptionFor(noCustomerEmail)).to.be.undefined;
       });
-      it('should update subscription if it exist', async function() {
-        await createSubscription(customerId, customerEmail, 3);
+
+      it('should create subscription for customer', async function() {
+        expect(await subscriptionFor(customerEmail)).to.be.undefined;
 
         await this.magentoApi.execute('subscriptions', 'update', {
-          subscriptions: [{ subscriber_email: customerEmail, subscriber_status: true }]
+          subscriptions: [
+            { subscriber_email: customerEmail, subscriber_status: true, customer_id: customerId, website_id: websiteId }
+          ]
         });
+
         expect(isSubscribed(await subscriptionFor(customerEmail))).to.be.true;
+      });
+
+      it('should subscribe all the subscriptions', async function() {
+        await createSubscription(GUEST_CUSTOMER_ID, noCustomerEmail, MAGENTO_UNSUBSCRIBE_STATUS);
+
+        await this.magentoApi.execute('subscriptions', 'update', {
+          subscriptions: [
+            {
+              subscriber_email: customerEmail,
+              subscriber_status: true,
+              customer_id: customerId,
+              website_id: websiteId
+            },
+            {
+              subscriber_email: customerEmail2,
+              subscriber_status: true,
+              customer_id: customerId2,
+              website_id: websiteId
+            },
+            {
+              subscriber_email: noCustomerEmail,
+              subscriber_status: true,
+              customer_id: GUEST_CUSTOMER_ID,
+              website_id: websiteId
+            }
+          ]
+        });
+
+        expect(isSubscribed(await subscriptionFor(customerEmail))).to.be.true;
+        expect(isSubscribed(await subscriptionFor(customerEmail2))).to.be.true;
+        expect(isSubscribed(await subscriptionFor(noCustomerEmail))).to.be.true;
       });
     });
 
@@ -71,30 +137,97 @@ describe('Subscriptions api', function() {
         expect(await subscriptionFor(noCustomerEmail)).to.be.undefined;
 
         await this.magentoApi.execute('subscriptions', 'update', {
-          subscriptions: [{ subscriber_email: noCustomerEmail, subscriber_status: false }]
+          subscriptions: [
+            {
+              subscriber_email: noCustomerEmail,
+              subscriber_status: false,
+              customer_id: GUEST_CUSTOMER_ID,
+              website_id: websiteId
+            }
+          ]
         });
 
         expect(await subscriptionFor(noCustomerEmail)).to.be.undefined;
       });
-      it('should unsubscribe', async function() {
-        await createSubscription(customerId, customerEmail, 1);
+
+      it('should update unsubscribed record for guest', async function() {
+        await createSubscription(GUEST_CUSTOMER_ID, noCustomerEmail, MAGENTO_SUBSCRIBE_STATUS);
+        expect(isSubscribed(await subscriptionFor(noCustomerEmail))).to.be.true;
+
         await this.magentoApi.execute('subscriptions', 'update', {
-          subscriptions: [{ subscriber_email: customerEmail, subscriber_status: false, customer_id: customerId }]
+          subscriptions: [
+            {
+              subscriber_email: noCustomerEmail,
+              subscriber_status: false,
+              customer_id: GUEST_CUSTOMER_ID,
+              website_id: websiteId
+            }
+          ]
+        });
+
+        expect(isSubscribed(await subscriptionFor(noCustomerEmail))).to.be.false;
+      });
+
+      it('should unsubscribe', async function() {
+        await createSubscription(customerId, customerEmail, MAGENTO_SUBSCRIBE_STATUS);
+
+        expect(isSubscribed(await subscriptionFor(customerEmail))).to.be.true;
+
+        await this.magentoApi.execute('subscriptions', 'update', {
+          subscriptions: [
+            {
+              subscriber_email: customerEmail,
+              subscriber_status: false,
+              customer_id: customerId,
+              website_id: websiteId
+            }
+          ]
         });
 
         expect(isSubscribed(await subscriptionFor(customerEmail))).to.be.false;
       });
+
+      it('should unsubscribe all the subscriptions', async function() {
+        await createSubscription(customerId, customerEmail, MAGENTO_SUBSCRIBE_STATUS);
+        await createSubscription(customerId2, customerEmail2, MAGENTO_SUBSCRIBE_STATUS);
+        await createSubscription(GUEST_CUSTOMER_ID, noCustomerEmail, MAGENTO_SUBSCRIBE_STATUS);
+
+        expect(isSubscribed(await subscriptionFor(customerEmail))).to.be.true;
+        expect(isSubscribed(await subscriptionFor(customerEmail2))).to.be.true;
+        expect(isSubscribed(await subscriptionFor(noCustomerEmail))).to.be.true;
+
+        await this.magentoApi.execute('subscriptions', 'update', {
+          subscriptions: [
+            {
+              subscriber_email: customerEmail,
+              subscriber_status: false,
+              customer_id: customerId,
+              website_id: websiteId
+            },
+            {
+              subscriber_email: customerEmail2,
+              subscriber_status: false,
+              customer_id: customerId2,
+              website_id: websiteId
+            },
+            {
+              subscriber_email: noCustomerEmail,
+              subscriber_status: false,
+              customer_id: GUEST_CUSTOMER_ID,
+              website_id: websiteId
+            }
+          ]
+        });
+
+        expect(isSubscribed(await subscriptionFor(customerEmail))).to.be.false;
+        expect(isSubscribed(await subscriptionFor(customerEmail2))).to.be.false;
+        expect(isSubscribed(await subscriptionFor(noCustomerEmail))).to.be.false;
+      });
     });
   });
   describe('get', function() {
-    let customerEmail2;
-    let customerId2;
-
     before(async function() {
       await this.db('newsletter_subscriber').delete();
-
-      customerEmail2 = this.customer.email;
-      customerId2 = this.customer.entityId;
 
       await this.db
         .insert([
@@ -115,14 +248,14 @@ describe('Subscriptions api', function() {
       const expectedSubscriptions = {
         subscriptions: [
           {
-            customer_id: 0,
+            customer_id: GUEST_CUSTOMER_ID,
             store_id: storeId,
             subscriber_email: noCustomerEmail,
             subscriber_status: '1',
             website_id: websiteId
           },
           {
-            customer_id: 0,
+            customer_id: GUEST_CUSTOMER_ID,
             store_id: storeId,
             subscriber_email: noCustomerEmail2,
             subscriber_status: '3',
@@ -156,7 +289,7 @@ describe('Subscriptions api', function() {
       const expectedSubscriptions = {
         subscriptions: [
           {
-            customer_id: 0,
+            customer_id: GUEST_CUSTOMER_ID,
             store_id: storeId,
             subscriber_email: noCustomerEmail,
             subscriber_status: '1',
@@ -188,7 +321,7 @@ describe('Subscriptions api', function() {
       const expectedSubscriptions = {
         subscriptions: [
           {
-            customer_id: 0,
+            customer_id: GUEST_CUSTOMER_ID,
             store_id: storeId,
             subscriber_email: noCustomerEmail2,
             subscriber_status: '3',
@@ -221,13 +354,13 @@ describe('Subscriptions api', function() {
       const expectedSubscriptions = {
         subscriptions: [
           {
-            customer_id: 0,
+            customer_id: GUEST_CUSTOMER_ID,
             store_id: storeId,
             subscriber_email: noCustomerEmail,
             subscriber_status: '1'
           },
           {
-            customer_id: 0,
+            customer_id: GUEST_CUSTOMER_ID,
             store_id: storeId,
             subscriber_email: noCustomerEmail2,
             subscriber_status: '3'
